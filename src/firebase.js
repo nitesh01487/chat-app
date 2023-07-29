3870d5b8d34b015624572e426c71ed30ff9bed61
nitesh01487 <niteshkumar01487@gmail.com>
2023-07-29 15:54:45 +0530

    chap final

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDRGGwvkgTDZo5M58qmP9I8PkZ0VRABiGg",
+  authDomain: "chatappnitesh01487.firebaseapp.com",
+  projectId: "chatappnitesh01487",
+  storageBucket: "chatappnitesh01487.appspot.com",
+  messagingSenderId: "561255329195",
+  appId: "1:561255329195:web:b45c9b44a03d6b38d836ee"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
